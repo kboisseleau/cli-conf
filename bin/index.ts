06fe6bff175fae0c3bfab6dd694e645827a04586
kboisseleau <boisseleau.kevin@gmail.com>
2023-04-07 14:23:53 +0200

    test

--- bin/index.ts
+++ bin/index.ts
@@ -2,8 +2,8 @@ import { Github } from '../src/lib/github.js'
 import chalk from 'chalk'
 import clear  from 'clear'
 import figlet  from 'figlet'
-import yargs from 'yargs'
-import { hideBin } from 'yargs/helpers'
+// import yargs from 'yargs'
+// import { hideBin } from 'yargs/helpers'
 
 clear()
 
@@ -15,10 +15,11 @@ console.log(
 
   const github = new Github()
   const run = async () => {
-    yargs(hideBin(process.argv))
-    .command('repo', 'create repo github', () => {
-      github.createRepo()
-    }).argv
+    github.createRepo()
+    // yargs(hideBin(process.argv))
+    // .command('repo', 'create repo github', () => {
+    //   github.createRepo()
+    // }).argv
   }
 
 run()
--- dist/bin/index.js
+++ dist/bin/index.js
@@ -2,26 +2,26 @@ import { Github } from '../src/lib/github.js';
 import chalk from 'chalk';
 import clear from 'clear';
 import figlet from 'figlet';
-import { Repos } from 'src/lib/repo.js';
+import { Repos } from '../src/lib/repo.js';
 clear();
 console.log(chalk.yellow(figlet.textSync("Gconf", { horizontalLayout: "full" })));
-const github = new Github();
+new Github();
 const run = async () => {
     try {
         const repo = new Repos();
         const url = await repo.createRemoteRepo();
         await repo.createGitignore();
         await repo.setupRepo(url);
-        console.log(chalk.green('All done!'));
+        console.log(chalk.green("All done!"));
     }
     catch (err) {
         if (err) {
             switch (err.status) {
                 case 401:
-                    console.log(chalk.red('Couldn\'t log you in. Please provide correct credentials/token.'));
+                    console.log(chalk.red("Couldn\"t log you in. Please provide correct credentials/token."));
                     break;
                 case 422:
-                    console.log(chalk.red('There is already a remote repository or token with the same name'));
+                    console.log(chalk.red("There is already a remote repository or token with the same name"));
                     break;
                 default:
                     console.log(chalk.red(err));
